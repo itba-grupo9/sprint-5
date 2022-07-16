d7efc92f2388fee8d1b8ee6cb76073863bde753f
Juan Ignacio Vila <jivila@outlook.com>
2022-07-16 12:55:49 -0300

    Clientes terminado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from components.clientes import Classic, Gold, Black
+from components.clientes import *
 
 data = {
 	"numero": 100001,
@@ -19,7 +19,7 @@ data = {
 		"cuentaNumero": 190,
 		"cupoDiarioRestante": 9000,
 		"cantidadExtraccionesHechas": 1,
-		"monto": 1000,
+		"monto": 100000000,
 		"fecha": "06/06/2022 10:00:55",
 		"numero": 1,
 		"saldoEnCuenta": 100000,
@@ -27,122 +27,128 @@ data = {
 		"totalChequerasActualmente": 2
 		}]
 	}
+#
+# def crear_tipo_cliente():
+# 	if cuenta.tipo == "BLACK":
+# 			return Black()
+#
+# 	if cuenta.tipo == "GOLD":
+# 			return Gold()
+#
+# 	if cuenta.tipo == "CLASSIC":
+# 			return Classic()
+#
+#
+# class ErrorChecker:
+# 	def __init__(self, cuenta):
+# 		self.cuenta = cuenta
+# 		self.chequeo_a_ejecutar()
+#
+# 	def chequeo_a_ejecutar(self):
+# 		if cuenta.tipo_de_transaccion == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO": self.__chequeo_retiro_efectivo()
+# 		if cuenta.tipo_de_transaccion == "ALTA_TARJETA_CREDITO": self.__chequeo_alta_tarjeta_credito()
+# 		if cuenta.tipo_de_transaccion == "ALTA_CHEQUERA": self.__chequeo_alta_chequera()
+# 		if cuenta.tipo_de_transaccion == "COMPRAR_DOLAR": self.__chequeo_compra_dolares()
+# 		if cuenta.tipo_de_transaccion == "TRANSFERENCIA_ENVIADA": self.__chequeo_transferencia_enviada()
+# 		if cuenta.tipo_de_transaccion == "TRANSFERENCIA_RECIBIDA": self.__chequeo_transferencia_recibida()
+#
+# 	# Errores posibles
+#
+# 	def __chequeo_retiro_efectivo(self):
+# 		"""- Chequea que el monto a retirar no sea mayor al disponible.
+# 		- Chequea que el monto a retirar no supere el limite de extraccion.
+# 		"""
+# 		if cuenta.monto > cuenta.saldo_en_cuenta:
+# 			return "El monto que se quiere retirar es mayor al que dispone en sus cuentas"
+# 		if cuenta.monto > tipo_de_cuenta.limite_extraccion:
+# 			return f"Su tipo de cuenta no permite un retiro mayor a ${tipo_de_cuenta.limite_extraccion}."
+#
+# 		return True
+#
+# 	def __chequeo_alta_tarjeta_credito(self):
+# 		"""- Chequea si el usuario puede crear una tarjeta de credito"""
+# 		if cuenta.tipo == "CLASSIC":
+# 			return "No es posible que los clientes Classic cuenten con tarjeta de credito. Por favor, actualicese a Gold o Black."
+#
+# 		if cuenta.total_tarjetas_de_credito_actualmente >= tipo_de_cuenta.limite_cantidad_tc:
+# 			return f"No es posible crear una tarjeta de credito ya que alcanzo el limite."
+#
+# 		return True
+#
+# 	def __chequeo_alta_chequera(self):
+# 		"""- Chequea si el usuario puede crear una chequera"""
+# 		if cuenta.tipo == "CLASSIC":
+# 			return "No es posible que los clientes Classic cuenten con una chequera. Por favor, actualicese a Gold o Black."
+#
+# 		if cuenta.total_tarjetas_de_credito_actualmente >= tipo_de_cuenta.limite_chequeras:
+# 			return f"No es posible crear una cheuquera ya que alcanzo el limite."
+#
+# 		return True
+#
+# 	def __chequeo_compra_dolares(self):
+# 		if cuenta.tipo == "Classic":
+# 			return "No es posible que los usuarios Classic realicen una compra de dolares. Por favor actualicese a Gold o Black"
+#
+# 		return True
+#
+# 	def __chequeo_transferencia_enviada(self):
+# 		if cuenta.monto + cuenta.monto * tipo_de_cuenta.comision_transferencias > cuenta.saldo_en_cuenta:
+# 			return "No es posible realizar la transferencia. Fondos insuficientes."
+#
+# 	def __chequeo_transferencia_recibida(self):
+# 		if cuenta.monto >= tipo_de_cuenta.limite_transf_sin_aviso:
+# 			return "No es posible recibir la transferencia ya que excede el limite de transferencias por dia sin aviso."
+#
+#
+# class ProcesadorDeInputs:
+# 	def __init__(self, data_input):
+# 		self.cliente = None
+# 		self.tipo = data_input["tipo"]
+# 		self.nombre = data_input['nombre']
+# 		self.apellido = data_input['apellido']
+# 		self.numero = data_input['numero']
+# 		self.dni = data_input['dni']
+#
+# 		# Direccion
+# 		self.calle = data_input["direccion"]["calle"]
+# 		self.numero = data_input["direccion"]["numero"]
+# 		self.ciudad = data_input['direccion']["ciudad"]
+# 		self.provincia = data_input["direccion"]["provincia"]
+# 		self.pais = data_input["direccion"]["pais"]
+#
+# 		# Transacciones
+# 		self.estado = data_input["transacciones"][0]["estado"]
+# 		self.tipo_de_transaccion = data_input["transacciones"][0]["tipo"]
+# 		self.cuenta_numero = data_input["transacciones"][0]["cuentaNumero"]
+# 		self.cupo_diario_restante = data_input["transacciones"][0]["cupoDiarioRestante"]
+# 		self.cantidad_extracciones_hechas = data_input["transacciones"][0]["cantidadExtraccionesHechas"]
+# 		self.monto = data_input["transacciones"][0]["monto"]
+# 		self.fecha = data_input['transacciones'][0]["fecha"]
+# 		self.numero = data_input['transacciones'][0]["numero"]
+# 		self.saldo_en_cuenta = data_input["transacciones"][0]["saldoEnCuenta"]
+# 		self.total_tarjetas_de_credito_actualmente = data_input["transacciones"][0]["totalTarjetasDeCreditoActualmente"]
+# 		self.total_chequeras_actualmente = data_input["transacciones"][0]["totalChequerasActualmente"]
+#
 
-def crear_tipo_cliente():
-	if cuenta_ingresada.tipo == "BLACK":
-			return Black()
-		
-	if cuenta_ingresada.tipo == "GOLD":
-			return Gold()
-		
-	if cuenta_ingresada.tipo == "CLASSIC":
-			return Classic()
-	
-		
-class ErrorChecker:
-	def __init__(self):
-		self.chequeo_a_ejecutar()
-	
-	def chequeo_a_ejecutar(self):
-		if cuenta_ingresada.tipo_de_transaccion == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO": self.__chequeo_retiro_efectivo()
-		if cuenta_ingresada.tipo_de_transaccion == "ALTA_TARJETA_CREDITO": self.__chequeo_alta_tarjeta_credito()
-		if cuenta_ingresada.tipo_de_transaccion == "ALTA_CHEQUERA": self.__chequeo_alta_chequera()
-		if cuenta_ingresada.tipo_de_transaccion == "COMPRAR_DOLAR": self.__chequeo_compra_dolares()
-		if cuenta_ingresada.tipo_de_transaccion == "TRANSFERENCIA_ENVIADA": self.__chequeo_transferencia_enviada()
-		if cuenta_ingresada.tipo_de_transaccion == "TRANSFERENCIA_RECIBIDA": self.__chequeo_transferencia_recibida()
-	
-	# Errores posibles
-	
-	def __chequeo_retiro_efectivo(self):
-		"""- Chequea que el monto a retirar no sea mayor al disponible.
-		- Chequea que el monto a retirar no supere el limite de extraccion.
-		"""
-		if cuenta_ingresada.monto > cuenta_ingresada.saldo_en_cuenta:
-			return "El monto que se quiere retirar es mayor al que dispone en sus cuentas"
-		if cuenta_ingresada.monto > cliente.limite_extraccion:
-			return f"Su tipo de cuenta no permite un retiro mayor a ${cliente.limite_extraccion}."
-		
-		return True
-	
-	def __chequeo_alta_tarjeta_credito(self):
-		"""- Chequea si el usuario puede crear una tarjeta de credito"""
-		if cuenta_ingresada.tipo == "CLASSIC":
-			return "No es posible que los clientes Classic cuenten con tarjeta de credito. Por favor, actualicese a Gold o Black."
-		
-		if cuenta_ingresada.total_tarjetas_de_credito_actualmente >= cliente.limite_cantidad_tc:
-			return f"No es posible crear una tarjeta de credito ya que alcanzo el limite."
-		
-		return True
-	
-	def __chequeo_alta_chequera(self):
-		"""- Chequea si el usuario puede crear una chequera"""
-		if cuenta_ingresada.tipo == "CLASSIC":
-			return "No es posible que los clientes Classic cuenten con una chequera. Por favor, actualicese a Gold o Black."
-		
-		if cuenta_ingresada.total_tarjetas_de_credito_actualmente >= cliente.limite_chequeras:
-			return f"No es posible crear una cheuquera ya que alcanzo el limite."
-		
-		return True
-	
-	def __chequeo_compra_dolares(self):
-		if cuenta_ingresada.tipo == "Classic":
-			return "No es posible que los usuarios Classic realicen una compra de dolares. Por favor actualicese a Gold o Black"
-		
-		return True
-	
-	def __chequeo_transferencia_enviada(self):
-		if cuenta_ingresada.monto + cuenta_ingresada.monto * cliente.comision_transferencias > cuenta_ingresada.saldo_en_cuenta:
-			return "No es posible realizar la transferencia. Fondos insuficientes."
-	
-	def __chequeo_transferencia_recibida(self):
-		if cuenta_ingresada.monto >= cliente.limite_transf_sin_aviso:
-			return "No es posible recibir la transferencia ya que excede el limite de transferencias por dia sin aviso."
-
-
-class ProcesadorDeInputs:
-	def __init__(self, data_input):
-		self.cliente = None
-		self.tipo = data_input["tipo"]
-		self.nombre = data_input['nombre']
-		self.apellido = data_input['apellido']
-		self.numero = data_input['numero']
-		self.dni = data_input['dni']
-		
-		# Direccion
-		self.calle = data_input["direccion"]["calle"]
-		self.numero = data_input["direccion"]["numero"]
-		self.ciudad = data_input['direccion']["ciudad"]
-		self.provincia = data_input["direccion"]["provincia"]
-		self.pais = data_input["direccion"]["pais"]
-		
-		# Transacciones
-		self.estado = data_input["transacciones"][0]["estado"]
-		self.tipo_de_transaccion = data_input["transacciones"][0]["tipo"]
-		self.cuenta_numero = data_input["transacciones"][0]["cuentaNumero"]
-		self.cupo_diario_restante = data_input["transacciones"][0]["cupoDiarioRestante"]
-		self.cantidad_extracciones_hechas = data_input["transacciones"][0]["cantidadExtraccionesHechas"]
-		self.monto = data_input["transacciones"][0]["monto"]
-		self.fecha = data_input['transacciones'][0]["fecha"]
-		self.numero = data_input['transacciones'][0]["numero"]
-		self.saldo_en_cuenta = data_input["transacciones"][0]["saldoEnCuenta"]
-		self.total_tarjetas_de_credito_actualmente = data_input["transacciones"][0]["totalTarjetasDeCreditoActualmente"]
-		self.total_chequeras_actualmente = data_input["transacciones"][0]["totalChequerasActualmente"]
-
-
-# 1. Procesamos los datos del cliente
-cuenta_ingresada = ProcesadorDeInputs(data)
-
-# 2. Creamos el tipo de cliente predefinido
-cliente = crear_tipo_cliente()
-
-# 3. Chequeamos errores
-print(ErrorChecker())
->>>>>>> 02f20e9 (Base de proyecto creada)
-
-
-
+# # 1. Procesamos los datos del tipo_de_cuenta
+# cuenta = ProcesadorDeInputs(data)
+#
+#
+#
+# # 2. Creamos el tipo de tipo_de_cuenta predefinido basado en los inputs de la cuenta
+# tipo_de_cuenta = crear_tipo_cliente()
+#
+# print(tipo_de_cuenta.caja_de_ahorro_pesos)
 
 
+# # 3. Chequeamos errores
+# error_checker = ErrorChecker(cuenta)
+# print(error_checker.chequeo_a_ejecutar())
 
 
 # TODO Revisar giro en descubierto
+
+
+cliente = Black()
+
+print(cliente.puede_crear_chequera())
--- components/clientes.py
+++ components/clientes.py
@@ -1,46 +1,14 @@
-# TODO Chequear objetos creados en las clases hijas
-# TODO Chequear si el dolar blue esta bien elegir para la compra
-
 # TODO Agregar clase Cuenta
 class Clientes:
-	
-	# TODO Agregar clase direccion aca
-	def __init__(self, nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais):
-		# Datos del usuario
-		self.nombre = nombre
-		self.apellido = apellido
-		self.numero = numero
-		self.dni = dni
-		
-		# Direccion del cliente
-		self.direccion = f"{calle} {numero_calle}, {ciudad}, {provincia}, {pais}."
-		self.calle = calle
-		self.numero_calle = numero_calle
-		self.ciudad = ciudad
-		self.provincia = provincia
-		self.pais = pais
-		
-		# Propiedades de la cuenta
-		self.tarjeta_debito = None
-		self.cuenta_corriente = None
-		self.cuenta_corriente_descubierto = None
-		self.caja_de_ahorro_pesos = None
-		self.caja_de_ahorro_dolares = None
-		self.tarjeta_credito = None
-		self.limite_cantidad_tc = None
-		self.limite_extraccion = None
-		self.limite_transf_sin_aviso = None
-		self.limite_chequeras = None
-		self.comision_transferencias = None
-	
+	# TODO Revisar las funciones
 	def puede_crear_chequera(self):
-		return True if self.limite_chequeras else False
+		return True if self.limite_chequeras > 0 else False
 	
 	def puede_crear_tarjeta_credito(self):
-		return True if self.tarjeta_credito else False
+		return True if self.limite_tarjetas_credito > 0 else False
 	
 	def puede_comprar_dolares(self):
-		return True if self.caja_de_ahorro_dolares == 0 else False
+		return True if self.caja_de_ahorro_dolares else False
 	
 	def retiro_efectivo_cajero_automatico(self, cantidad_a_retirar):
 		"""Revisa si el usuario dispone saldo en cuenta corriente y caja de ahorro y realiza los retiros por cajero"""
@@ -51,73 +19,70 @@ class Clientes:
 			return f"El monto de retiro no puede superar el limite de extraccion. Su limite de extraccion es de ${self.limite_extraccion} y esta intentando " \
 			       f"retirar ${cantidad_a_retirar}."
 		# No supere el giro descubierto
-		if (self.caja_de_ahorro_pesos - cantidad_a_retirar) < -self.cuenta_corriente_descubierto:
+		if (self.caja_de_ahorro_en_pesos - cantidad_a_retirar) < -self.cuenta_corriente_descubierto:
 			return f"No es posible realizar el retiro, ya que su giro en descubierto tiene un limite de {self.cuenta_corriente_descubierto}."
 		
 		# Funcion a ejecutar
-		self.caja_de_ahorro_pesos -= cantidad_a_retirar
-		print(f"La cantidad reditarda es de {cantidad_a_retirar}, el saldo de la caja de ahorro es de {self.caja_de_ahorro_pesos}")
+		self.caja_de_ahorro_en_pesos -= cantidad_a_retirar
+		print(f"La cantidad reditarda es de {cantidad_a_retirar}, el saldo de la caja de ahorro es de {self.caja_de_ahorro_en_pesos}")
 	
 	def alta_tarjeta_credito(self):
 		"""Revisa si el usuario puede dar de alta una tarjeta de credito y si cumple con los requisitos la crea"""
 		
-		if not self.limite_cantidad_tc: return f"No es posible asignarle una tarjeta de credito a {self.apellido}"
+		if not self.limite_tarjetas_credito: return f"No es posible asignarle una tarjeta de credito."
 		
-		self.limite_cantidad_tc -= 1
-		return f"La tarjeta de credito de {self.nombre} {self.apellido} fue creada perfectamente. Tarjetas restantes: {self.limite_cantidad_tc}."
+		self.limite_tarjetas_credito -= 1
+		return f"La tarjeta de credito fue creada perfectamente. Tarjetas restantes: {self.limite_tarjetas_credito}."
 	
 	def alta_chequera(self):
 		"""Revisa si el usuario puede crear una chequera y si cumple los requisitos la crea"""
 		
-		if not self.puede_crear_chequera(): return f"No es posible asignarle una chequera a {self.apellido}."
+		if not self.puede_crear_chequera(): return f"No es posible asignarle una chequera."
 		
 		self.limite_chequeras -= 1
-		return f"La chequera de {self.nombre} {self.apellido} fue creada correctamente. Chequeras restantes: {self.limite_chequeras}."
+		return f"La chequera fue creada correctamente. Chequeras restantes: {self.limite_chequeras}."
 
 
 class Classic(Clientes):
-	def __init__(self, nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais):
-		super().__init__(nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais)
-		self.tarjeta_debito = True
-		self.cuenta_corriente = 0
-		self.cuenta_corriente_descubierto = 0
-		self.caja_de_ahorro_pesos = 0
-		self.caja_de_ahorro_dolares = None
-		self.tarjeta_credito = None
-		self.limite_cantidad_tc = 0
-		self.limite_extraccion = 10000
-		self.limite_transf_sin_aviso = 150000
-		self.limite_chequeras = None
+	def __init__(self):
+		super().__init__()
+		self.caja_de_ahorro_en_pesos = True
+		self.caja_de_ahorro_en_dolares = False
+		self.cuenta_corriente = False
+		self.maximo_retiro = 10000
+		self.limite_tarjetas_debito = 1
+		self.limite_tarjetas_credito = 0
+		self.limite_chequeras = 0
+		self.limite_recibo_transferencia = 150000
 		self.comision_transferencias = 0.01
+		self.giro_descubierto = 0
 
 
 class Gold(Clientes):
-	def __init__(self, nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais):
-		super().__init__(nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais)
-		self.tarjeta_debito = True
-		self.cuenta_corriente = 0
-		self.cuenta_corriente_descubierto = -10000
-		self.caja_de_ahorro_pesos = 0
-		self.caja_de_ahorro_dolares = 0
-		self.tarjeta_credito = True
-		self.limite_cantidad_tc = 1
-		self.limite_extraccion = 20000
-		self.limite_transf_sin_aviso = 500000
+	def __init__(self):
+		super().__init__()
+		self.caja_de_ahorro_en_pesos = True
+		self.caja_de_ahorro_en_dolares = True
+		self.cuenta_corriente = True
+		self.maximo_retiro = 20000
+		self.limite_tarjetas_debito = 1
+		self.limite_tarjetas_credito = 1
 		self.limite_chequeras = 1
+		self.limite_recibo_transferencias = 500000
 		self.comision_transferencias = 0.005
+		self.giro_descubierto = 10000
 
 
 class Black(Clientes):
-	def __init__(self, nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais):
-		super().__init__(nombre, apellido, numero, dni, calle, numero_calle, ciudad, provincia, pais)
-		self.tarjeta_debito = True
-		self.cuenta_corriente = 0
-		self.cuenta_corriente_descubierto = -10000
-		self.caja_de_ahorro_pesos = 0
-		self.caja_de_ahorro_dolares = 0
-		self.tarjeta_credito = True
-		self.limite_cantidad_tc = 5
-		self.limite_extraccion = 100000
-		# self.limite_transf_sin_aviso = // TODO revisar como hacerlo infinito
+	def __init__(self):
+		super().__init__()
+		self.caja_de_ahorro_en_pesos = True
+		self.caja_de_ahorro_en_dolares = True
+		self.cuenta_corriente = True
+		self.maximo_retiro = 100000
+		self.limite_tarjetas_debito = 1
+		self.limite_tarjetas_credito = 5
 		self.limite_chequeras = 2
+		self.limite_recibo_transferencias = float('inf')
 		self.comision_transferencias = 0
+		self.giro_descubierto = 10000
